02befe4a79ca66797f3deec835870ffe04442f68
ejsultan <ejsultan@gmail.com>
2026-02-04 19:51:19 -0800

    Fix Gemini-cli install script.

--- docs/Scripts/install_gemini-cli.sh
+++ docs/Scripts/install_gemini-cli.sh
@@ -1,8 +1,8 @@
 # Update NodeSource for Node.js 20, install Node.js, and install Gemini CLI
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - && \
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt update -y
-sudo apt install -y nodejs && \
-sudo npm install -g @google/gemini-cli && \
-echo 'export PATH=$PATH:$(npm config get prefix)/bin' >> ~/.bashrc && \
-source ~/.bashrc && \
+sudo apt install -y nodejs
+sudo npm install -g @google/gemini-cli
+echo 'export PATH=$PATH:$(npm config get prefix)/bin' >> ~/.bashrc
+source ~/.bashrc
 echo "Installation complete. Type 'gemini' to start."
